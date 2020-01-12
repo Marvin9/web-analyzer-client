eb1877c0350687a8f823e2d2b88bd1860e0b74b4
Mayursinh <marvinduff97@gmail.com>
2020-01-12 23:02:20 +0530

    identify error - add loader

--- src/Components/seo-home/index.js
+++ src/Components/seo-home/index.js
@@ -4,22 +4,46 @@ import Report from './Components/Report';
 
 export default function index() {
   const [report, changeReport] = useState([]);
+  const [loader, changeLoader] = useState(false);
+
   const api = 'http://localhost:3000/api';
+
   const getReport = (url) => {
+    changeLoader(true);
     fetch(`${api}/analyze?url=${url}`)
       .then((res) => res.json())
       .then((generatedReport) => {
-        changeReport([...generatedReport]);
+        if (generatedReport.error) { } // todo handle error
+        else changeReport([...generatedReport]);
       })
       .catch((err) => {
         throw err;
+      })
+      .finally(() => {
+        changeLoader(false);
       });
   };
 
   return (
     <>
       <GetURL dispatchUrl={getReport} />
-      <Report report={report} />
+      { loader
+        ? (
+          <div className="d-flex justify-content-center">
+            <div
+              className="spinner-border"
+              role="status"
+              style={{
+                width: '4rem',
+                height: '4rem'
+              }}
+            >
+              <span className="sr-only">Loading...</span>
+            </div>
+          </div>
+        )
+        : <Report report={report} />
+      }
     </>
   );
 }
