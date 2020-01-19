e0d31b71d69bc0a977370ad001f1f357965d6248
Mayursinh <marvinduff97@gmail.com>
2020-01-19 10:39:32 +0530

    fix deepscan issue

--- src/Components/seo-rules/Components/List.js
+++ src/Components/seo-rules/Components/List.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 const List = ({ rulesList }) => (
   <>
     { rulesList.map((rule) => (
-      <div className="card mb-4" style={{ cursor: 'pointer' }}>
+      <div key={`${rule.ruleName}_${rule.tag}`} className="card mb-4" style={{ cursor: 'pointer' }}>
         <div className="card-body">
           <h5 className="card-title">
             Rule name :&nbsp;
