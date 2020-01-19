815a63306ecf6386f41c340615fa7743c5bf8b6f
Mayursinh <marvinduff97@gmail.com>
2020-01-19 10:36:01 +0530

    use Spinner & serverURL

--- src/Components/seo-home/index.js
+++ src/Components/seo-home/index.js
@@ -1,13 +1,15 @@
 import React, { useState } from 'react';
 import GetURL from './Components/GetURL';
 import Report from './Components/Report';
+import apiURL from '../../../serverURL';
+import Spinner from '../Spinner';
 
 export default function index() {
   const [report, changeReport] = useState([]);
   const [loader, changeLoader] = useState(false);
   const [error, changeError] = useState({ err: false, url: '' });
 
-  const api = 'http://localhost:3000/api';
+  const api = apiURL;
 
   const getReport = (url) => {
     changeLoader(true);
@@ -36,20 +38,7 @@ export default function index() {
     <>
       <GetURL dispatchUrl={getReport} />
       { loader
-        ? (
-          <div className="d-flex justify-content-center">
-            <div
-              className="spinner-border"
-              role="status"
-              style={{
-                width: '4rem',
-                height: '4rem',
-              }}
-            >
-              <span className="sr-only">Loading...</span>
-            </div>
-          </div>
-        )
+        ? <Spinner />
         : <Report report={report} error={error} /> }
     </>
   );
