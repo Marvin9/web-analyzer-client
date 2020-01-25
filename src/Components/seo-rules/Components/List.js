849b5ed3e965641f4b0268744741798063cad811
Mayursinh <marvinduff97@gmail.com>
2020-01-25 12:37:00 +0530

    :lipstick: Change UI of /rules router

--- src/Components/seo-rules/Components/List.js
+++ src/Components/seo-rules/Components/List.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import colors from '../../../themeStore';
 
 const List = ({ rulesList }) => (
   <>
     { rulesList.map((rule) => (
-      <div key={`${rule.ruleName}_${rule.tag}`} className="card mb-4" style={{ cursor: 'pointer' }}>
+      <div
+        key={`${rule.ruleName}_${rule.tag}`}
+        className="card mb-4"
+        style={{
+          cursor: 'pointer',
+          background: colors.royal,
+          color: '#fff',
+          boxShadow: 'inset -4px 4px 16px -6px rgba(0,0,0,0.41)',
+        }}
+      >
         <div className="card-body">
           <h5 className="card-title">
             Rule name :&nbsp;
